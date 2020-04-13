ac99dd191af3fba0a42fc7d70ef4d8d5319f3ea4
ShawnLee <54459770@qq.com>
2020-04-14 01:25:27 +0800

    Add Base Code

---
+++ CPlusPlusCheckr/Singleton/Singleton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Singleton<T> where T : new()
+{
+	private static T _instance;
+	public static T Ins()
+	{
+		if (null == _instance)
+		{
+			_instance = new T();
+		}
+
+		return _instance;
+	}
+
+	public bool CheckStringValid(string strValue)
+	{
+		if (null == strValue)
+			return false;
+
+		if (strValue.Length <= 0)
+			return false;
+
+		return true;
+	}
+
+	public bool CheckListValid<T1>(List<T1> listValue)
+	{
+		if (null == listValue)
+			return false;
+
+		if (listValue.Count <= 0)
+			return false;
+
+		return true;
+	}
+
+	public bool CheckArrayValid<T1>(T1[] arrValue)
+	{
+		if (null == arrValue)
+			return false;
+
+		if (arrValue.Length <= 0)
+			return false;
+
+		return true;
+	}
+};
